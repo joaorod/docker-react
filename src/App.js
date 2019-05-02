9c1a2ac2989328801347fd50ff170d288e61fe22
joao.rodrigues <joao.rodrigues@farfetch.com>
2019-05-02 14:21:02 +0100

    changed at feature f1

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Changed on Feature Branch!
         </a>
       </header>
     </div>
